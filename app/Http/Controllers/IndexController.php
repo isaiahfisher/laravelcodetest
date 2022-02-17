ce88ebc29b96bec241ffb65272fd1f2f7d93a2cb
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-02-17 18:18:55 -0500

    Adjust routing and add the raw query to the controller

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Sweetwater_test;
+
+class IndexController extends Controller
+{
+    function __invoke()
+    {
+        $comments = DB::table('Sweetwater_test')->selectRaw("orderid, comments, shipdate_expected AS date, 
+        IF((comments LIKE '%candy%' OR comments LIKE '%smarties%'), 'candy', IF((comments LIKE '%call%' OR comments LIKE '%contact%'), 'contact preferences', 
+        IF((comments LIKE '%signature%' OR comments LIKE '%sign%'), 'Delivery Signature', IF(comments LIKE '%referred%', 'referrals', 'Miscellaneous')))) AS classification"
+        )->orderBy('classification')->get();
+        return view('index')->with('reportData', $reportData);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::Controller('/', 'index');
+Route::get('/', IndexController::class);
