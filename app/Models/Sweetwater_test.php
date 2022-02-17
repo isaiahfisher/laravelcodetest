7caa678effe45624087557f00defeb090dbe8a54
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-02-17 17:24:46 -0500

    Adjust Eloquent models to fit existing sweetwater database

---
+++ app/Models/Sweetwater_test.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sweetwater_test extends Model
+{
+    protected $table = 'sweetwater_test';
+    public $timestamps = false;
+    protected $primaryKey = "orderid";
+
+}
