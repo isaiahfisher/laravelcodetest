a7a9c2c8e3c0ea53734b78762249981b7df57fe1
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-02-17 19:35:43 -0500

    index now returns a formatted table of OrderInfo ordered by comment catagory

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,16 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Sweetwater_test;
+use DB;
 
 class IndexController extends Controller
 {
     function __invoke()
     {
-        $comments = DB::table('Sweetwater_test')->selectRaw("orderid, comments, shipdate_expected AS date, 
+        $OrderInfo = DB::table('Sweetwater_test')->selectRaw("orderid, comments, shipdate_expected AS date, 
         IF((comments LIKE '%candy%' OR comments LIKE '%smarties%'), 'candy', IF((comments LIKE '%call%' OR comments LIKE '%contact%'), 'contact preferences', 
         IF((comments LIKE '%signature%' OR comments LIKE '%sign%'), 'Delivery Signature', IF(comments LIKE '%referred%', 'referrals', 'Miscellaneous')))) AS classification"
         )->orderBy('classification')->get();
 
-        return view('index')->with('comments', $comments);
+        return view('index')->with('OrderInfo', $OrderInfo);
     }
 }
