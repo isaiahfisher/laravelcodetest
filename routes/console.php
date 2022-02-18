a1eca982e2cbc52911865b7dcbcabe978db35657
Isaiah Fisher <isaiahfisher90@gmail.com>
2022-02-17 20:41:16 -0500

    add Artisan command to fix the shipping dates

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\Sweetwater_test;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,24 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('fix:dates', function (){
+
+    foreach(Sweetwater_test::where('comments', 'rlike', '[0-9]+/[0-9]+/[0-9]+')->get() as $OrderInfo)
+    {
+        $comment = $OrderInfo->comments;
+        preg_match('#\d{1,2}/\d{1,2}/\d{2,4}+#', $comment, $matches);
+        $shipment_date_str = $matches[0];
+        $shipment_date = new DateTime($shipment_date_str);
+        $shipment_date = $shipment_date->format('Y-m-d');
+        $OrderInfo->shipdate_expected = $shipment_date;
+        try {
+        $OrderInfo->save();
+        } catch (\Throwable $e)
+        {
+            $this->error("An error occurred while trying to fix the dates! Please check orderid: " . $OrderInfo->orderid);
+        }
+    }
+    $this->info("Successfully fixed expected shipping dates");
+
+})->purpose('pull shipping dates from comments and apply the dates to the order info comment field');
